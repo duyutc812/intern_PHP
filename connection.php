17503412796ff80c72bb9375bd88927db5704c6f
duyutc812 <65037887+duyutc812@users.noreply.github.com>
2020-12-30 10:18:30 +0700

    fix connection.php and post_model.php

--- connection.php
+++ connection.php
@@ -13,15 +13,20 @@
                 die("Connection failed: " .$this->conn->connect_error());
             }
             //create database
-            $sql_create_db = 'create database '.$this->database_intern;
-            if ($this->conn->query($sql_create_db)){
-                echo "Database created successfully!";
+            $db_selected = mysqli_select_db($this->conn, $this->database_intern);
+            // check database exists
+            if (!$db_selected) {
+                $sql_create_db = 'create database '.$this->database_intern;
+                $this->conn->query($sql_create_db);
+                $this->conn->set_charset('UTF-8');
             }
-            $this->conn->set_charset('UTF-8');
-            //  create table
+             // create table
             $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->database_intern);
-            if($this->conn){
-                $sql = "CREATE TABLE db_posts (
+            // check table exists
+            $table_exists = $this->conn->query("SELECT * FROM db_post");
+            if($table_exists == FALSE)
+            {
+               $sql = "CREATE TABLE db_posts (
                     id INT(6) AUTO_INCREMENT PRIMARY KEY,
                     title VARCHAR(255) NOT NULL,
                     description text,
@@ -34,9 +39,6 @@
                     echo "Table db_post created successfully";
                 }
             }
-            else {
-                die("Connection failed: " .$this->conn->connect_error());
-            }
         }
 
         // close
@@ -45,5 +47,52 @@
                 $this->conn.close();
             }
         }
+
+        public function sql_getPost() {
+            $sqlGetPost = "SELECT * FROM db_posts";
+            return $sqlGetPost;
+        }
+
+        public function sql_getDetailPost($id_post) {
+            $sqlGetDetailPost = "SELECT * FROM db_posts WHERE id = $id_post";
+            return $sqlGetDetailPost;
+        }
+
+        public function sql_addPost($title, $description, $image, $status, $create_at, $update_at) {
+            $sqlAddPost = "INSERT INTO db_posts VALUES('', '$title', '$description', '$image', '$status', '$create_at', '$update_at')";
+            return $sqlAddPost;
+        }
+
+        public function sql_updatePost($id_post, $title, $description, $image, $status, $update_at) {
+            $sqlUpdatePost = "UPDATE db_posts set title = '$title', description = '$description', image = '$image', status = '$status', update_at = '$update_at' WHERE id = $id_post;
+            ";
+            return $sqlUpdatePost;
+        }
+
+        public function sql_deletePost($id_post) {
+            $sqlDeletePost = "DELETE FROM db_posts WHERE id = $id_post";
+            return $sqlDeletePost;
+        }
+
+        public function sql_postPaginator($key_onPage, $rec_onPage) {
+            $sqlPostPaginator = "SELECT * FROM db_posts ORDER BY id ASC LIMIT $key_onPage, $rec_onPage";
+            return $sqlPostPaginator;
+        }
+
+         public function insertDataPost($create_at, $update_at) {
+            $sqlInsertData = "INSERT into db_posts VALUES ('', 'title1', 'description1', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
+                                                ('', 'title2', 'description2', 'abc.jpg', 2, '$this->create_at', '$this->update_at'), 
+                                                ('', 'title3', 'description3', 'abc.jpg', 1, '$this->create_at', '$this->update_at'), 
+                                                ('', 'title4', 'description4', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
+                                                ('', 'title5', 'description5', 'image.jpg', 2, '$this->create_at', '$this->update_at'), 
+                                                ('', 'title6', 'description6', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
+                                                ('', 'title7', 'description7', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
+                                                ('', 'title8', 'description8', 'abc.jpg', 1, '$this->create_at', '$this->update_at'), 
+                                                ('', 'title9', 'description9', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
+                                                ('', 'title10', 'description10', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
+                                                ('', 'title11', 'description11', 'image.jpg', 1, '$this->create_at', '$this->update_at')";
+            return $sqlInsertData;
+        }
+
     }
 ?>
--- models/post_model.php
+++ models/post_model.php
@@ -15,29 +15,22 @@
 
         // get all post
         public function getAllPost() {
-            $sql = "SELECT * FROM db_posts";
-            $result = $this->db->conn->query($sql);
+            $result = $this->db->conn->query($this->db->sql_getPost());
             return $result;
         }
 
-        // get detail post 
+        // get detail post
         public function getDetailPost($id_post) {
-            $sql = "SELECT * FROM db_posts WHERE id = $id_post";
-            $result = $this->db->conn->query($sql);
+            $result = $this->db->conn->query($this->db->sql_getDetailPost($id_post));
             return $result;
         }
 
         // add post
         public function addPost($title, $description, $image, $status) {
-            $this->title = $title;
-            $this->description = $description;
-            $this->image = $image;
-            $this->status = $status;
             $this->create_at = date('Y-m-d H-i-s');
             $this->update_at = date('Y-m-d H-i-s');
-            $sql = "INSERT INTO db_posts VALUES('', '$this->title', '$this->description', '$this->image', '$this->status', '$this->create_at', '$this->update_at')";
-            if ($this->db->conn->query($sql)) {
-                header('location:../intern_PHP/admin');
+            if ($this->db->conn->query($this->db->sql_addPost($title, $description, $image, $status, $this->create_at, $this->update_at))) {
+                header('Location: http://localhost/intern_PHP/admin');
             }
             else {
                 die("no result add!");
@@ -45,18 +38,11 @@
 
         }
 
-
         // update post
         public function updatePost($id_post, $title, $description, $image, $status) {
-            $this->title = $title;
-            $this->description = $description;
-            $this->image = $image;
-            $this->status = $status;
             $this->update_at = date('Y-m-d H-i-s');
-            $sql = "UPDATE db_posts set title = '$this->title', description = '$this->description', image = '$this->image', status = '$this->status', update_at = '$this->update_at' WHERE id = $id_post;
-            ";
-            if ($this->db->conn->query($sql)) {
-                header('location:../intern_PHP/admin');
+            if ($this->db->conn->query($this->db->sql_updatePost($id_post, $title, $description, $image, $status, $this->update_at))) {
+                header('Location: http://localhost/intern_PHP/admin');
             }
             else {
                 die("no result update!");
@@ -65,9 +51,8 @@
 
         // delete post
         public function delPost($id_post) {
-            $sql = "DELETE FROM db_posts WHERE id = $id_post";
-            if ($this->db->conn->query($sql)) {
-                header('location:../intern_PHP/admin');
+            if ($this->db->conn->query($this->db->sql_deletePost($id_post))) {
+                header('Location: http://localhost/intern_PHP/admin');
             }
             else {
                 die("no result delete!");
@@ -76,8 +61,7 @@
 
         // Post - Paginator
         public function postNavigation($key_onPage, $rec_onPage) {
-            $sql = "SELECT * FROM db_posts ORDER BY id ASC LIMIT $key_onPage, $rec_onPage";
-            $result = $this->db->conn->query($sql);
+            $result = $this->db->conn->query($this->db->sql_postPaginator($key_onPage, $rec_onPage));
             return $result;
         }
 
@@ -85,19 +69,8 @@
         public function insertData() {
             $this->create_at = date('Y-m-d H-i-s');
             $this->update_at = date('Y-m-d H-i-s');
-            $sql = "INSERT into db_posts VALUES ('', 'title1', 'description1', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
-                                                ('', 'title2', 'description2', 'abc.jpg', 2, '$this->create_at', '$this->update_at'), 
-                                                ('', 'title3', 'description3', 'abc.jpg', 1, '$this->create_at', '$this->update_at'), 
-                                                ('', 'title4', 'description4', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
-                                                ('', 'title5', 'description5', 'image.jpg', 2, '$this->create_at', '$this->update_at'), 
-                                                ('', 'title6', 'description6', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
-                                                ('', 'title7', 'description7', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
-                                                ('', 'title8', 'description8', 'abc.jpg', 1, '$this->create_at', '$this->update_at'), 
-                                                ('', 'title9', 'description9', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
-                                                ('', 'title10', 'description10', 'image.jpg', 1, '$this->create_at', '$this->update_at'), 
-                                                ('', 'title11', 'description11', 'image.jpg', 1, '$this->create_at', '$this->update_at')";
-            if ($this->db->conn->query($sql)) {
-                header('location:../intern_PHP/');
+            if ($this->db->conn->query($this->db->insertDataPost($this->create_at, $this->update_at))) {
+                header('Location: http://localhost/intern_PHP/');
             }
             else {
                 die("no result insert!");
